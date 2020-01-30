989a3af9d5e629a7153396bf84a92b87e5a214a6
asdf-2345 <52937308+asdf-2345@users.noreply.github.com>
2020-01-30 14:18:30 +0900

    색깔을 비교하는 방식을 바꿨습니다.
    이제 색깔을 미리 바꿔놓고 비교를 시작합니다.

--- 사진비교_비슷한걸찾음/사진비교_비슷한걸찾음/MainForm.cs
+++ 사진비교_비슷한걸찾음/사진비교_비슷한걸찾음/MainForm.cs
@@ -76,9 +76,10 @@ namespace 사진비교_비슷한걸찾음
 			string str = "";
 			Bitmap image1 = new Bitmap(pictureBox1.Image);
 			Bitmap image2 = new Bitmap(pictureBox2.Image);
+			Bitmap[] image = {image1, image2};
+			image = photoConversion(image1, image2);
 			for(int a = 0; a < 4; a++){
-				Bitmap[] image = {image1, image2};
-				image = reducedResolution(image1, image2);
+				image[1] = image2;
 				str = imageComparison(image[0], image[1]);
 				if(str != "다릅니다."){
 					break;
@@ -96,7 +97,7 @@ namespace 사진비교_비슷한걸찾음
 			Bitmap image2 = new Bitmap(pictureBox2.Image);
 			for(int a = 0; a < 4; a++){
 				Bitmap[] image = {image1, image2};
-				image = reducedResolution(image1, image2);
+				image = photoConversion(image1, image2);
 				str = imageComparison(image[0], image[1]);
 				if(str != "다릅니다."){
 					break;
@@ -124,9 +125,7 @@ namespace 사진비교_비슷한걸찾음
 				for (int j = 0; j < image1.Height; j++){
 					Image1 = image1.GetPixel(i, j).ToString();
 					Image2 = image2.GetPixel(i, j).ToString();
-					int[] Image1Color = findColors(Image1);
-					int[] Image2Color = findColors(Image2);
-					if (false == colorComparison(Image1Color, Image2Color)){
+					if(Image1 != Image2){
 						count++;
 					}
 				}
@@ -143,37 +142,38 @@ namespace 사진비교_비슷한걸찾음
 				}
 			}
 		}
-		bool colorComparison(int[] Image1Color, int[] Image2Color){
-			int image1 = Image1Color[0] + Image1Color[1] + Image1Color[2];
-			int image2 = Image2Color[0] + Image2Color[1] + Image2Color[2];
-			if(image1 - image2 > (3 * comparisonNum) || image2 - image1 > (3 * comparisonNum)){
-				return false;
-			}
-			for(int a = 0; a < 3; a++){
-				if(Image1Color[a] - Image2Color[a] > (2 * comparisonNum) || Image2Color[a] - Image1Color[a] > (2 * comparisonNum)){
-					Console.WriteLine((Image1Color[a] - Image2Color[a]) +" "+ (Image2Color[a] - Image1Color[a]));
-					return false;
-				}
-			}
-			return true;
-		}
-		int[] findColors(string input){
-			//색깔값은 Color [A=255, R=114, G=93, B=8] 이런식으로 되어있음
-			//얼마나 투명한지 표현하는 A값은 보지 않음 왜냐면 귀찮거든
-			int RPoint = input.IndexOf("R");
-			int GPoint = input.IndexOf("G");
-			int BPoint = input.IndexOf("B");
-			int R = int.Parse(input.Substring(RPoint+2, input.IndexOf(",", RPoint)-RPoint-2));
-			Console.WriteLine(R);
-			int G = int.Parse(input.Substring(GPoint+2, input.IndexOf(",", GPoint)-GPoint-2));
-			Console.WriteLine(G);
-			int B = int.Parse(input.Substring(BPoint+2, input.IndexOf("]", BPoint)-BPoint-2));
-			Console.WriteLine(B);
-			int[] Color = {R, G, B};
-			return Color;
-		}
+		//bool colorComparison(int[] Image1Color, int[] Image2Color){
+		//	int image1 = Image1Color[0] + Image1Color[1] + Image1Color[2];
+		//	int image2 = Image2Color[0] + Image2Color[1] + Image2Color[2];
+		//	if(image1 - image2 > (3 * comparisonNum) || image2 - image1 > (3 * comparisonNum)){
+		//		return false;
+		//	}
+		//	for(int a = 0; a < 3; a++){
+		//		if(Image1Color[a] - Image2Color[a] > (2 * comparisonNum) || Image2Color[a] - Image1Color[a] > (2 * comparisonNum)){
+		//			Console.WriteLine((Image1Color[a] - Image2Color[a]) +" "+ (Image2Color[a] - Image1Color[a]));
+		//			return false;
+		//		}
+		//	}
+		//	return true;
+		//}
 		
-		Bitmap[] reducedResolution(Bitmap image1, Bitmap image2){ //이미지 두개를 같은크기로 만들어줌
+		//int[] findColors(string input){ // Color.R, Color.G, Color.B로 표현할 수 있었네 아 ㅋㅋ
+		//	//색깔값은 Color [A=255, R=114, G=93, B=8] 이런식으로 되어있음
+		//	//얼마나 투명한지 표현하는 A값은 보지 않음 왜냐면 귀찮거든
+		//	int RPoint = input.IndexOf("R");
+		//	int GPoint = input.IndexOf("G");
+		//	int BPoint = input.IndexOf("B");
+		//	int R = int.Parse(input.Substring(RPoint+2, input.IndexOf(",", RPoint)-RPoint-2));
+		//	Console.WriteLine(R);
+		//	int G = int.Parse(input.Substring(GPoint+2, input.IndexOf(",", GPoint)-GPoint-2));
+		//	Console.WriteLine(G);
+		//	int B = int.Parse(input.Substring(BPoint+2, input.IndexOf("]", BPoint)-BPoint-2));
+		//	Console.WriteLine(B);
+		//	int[] Color = {R, G, B};
+		//	return Color;
+		//}
+		
+		Bitmap[] photoConversion(Bitmap image1, Bitmap image2){ //이미지 두개를 같은크기로 만들어줌
 			int _width = Math.Max(image1.Width, image2.Width);
 			int _height = Math.Max(image1.Height, image2.Height);
 			int width = ReSize(_width); //더 크기가 큰 영상을 기준으로 크기를 변경
@@ -181,13 +181,35 @@ namespace 사진비교_비슷한걸찾음
 			
 			Size size = new Size(width, height);
 			Bitmap resizeImage1 = new Bitmap(image1, size);
-			pictureBox1.Image = resizeImage1;
+			//pictureBox1.Image = resizeImage1;
 			Bitmap resizeImage2 = new Bitmap(image2, size);
-			pictureBox2.Image = resizeImage2;
-			Bitmap[] resizeImage = {resizeImage1, resizeImage2};
+			//pictureBox2.Image = resizeImage2;
+			//recolorImage1 =
+			ReColor(resizeImage1, resizeImage2);
+			Bitmap[] resizeImage = ReColor(resizeImage1, resizeImage2);
 			return resizeImage;
 		}
 		
+		Bitmap[] ReColor(Bitmap img1, Bitmap img2){
+			Color img1Color;
+			Color img2Color;
+			double comparisonNum2 = (comparisonNum * 0.1);
+			Console.WriteLine(comparisonNum2);
+			for (int i = 0; i < img1.Width; i++){
+				for (int j = 0; j < img2.Height; j++){
+					img1Color = img1.GetPixel(i, j);
+					img2Color = img2.GetPixel(i, j);
+					
+					img1Color = Color.FromArgb((int)Math.Truncate(img1Color.R*comparisonNum2), (int)Math.Truncate(img1Color.G*comparisonNum2), (int)Math.Truncate(img1Color.B*comparisonNum2));
+					img2Color = Color.FromArgb((int)Math.Truncate(img2Color.R*comparisonNum2), (int)Math.Truncate(img2Color.G*comparisonNum2), (int)Math.Truncate(img2Color.B*comparisonNum2));
+					
+					img1.SetPixel(i, j, img1Color);
+					img2.SetPixel(i, j, img2Color);
+				}
+			}
+			Bitmap[] img = {img1, img2};
+			return img;
+		}
 		int ReSize(int size){ //해상도를 낮추는 함수
 			if(size > 2000){
 				return (size / (20 * comparisonNum));
