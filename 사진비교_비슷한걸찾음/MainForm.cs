ce03309e590335cabbb11087f90e5a29cb459739
asdf-2345 <kiaaa741@gmail.com>
2020-01-31 15:45:26 +0900

    색 단순화를 할때 들어가는 색깔을 추가하였습니다.

--- 사진비교_비슷한걸찾음/MainForm.cs
+++ 사진비교_비슷한걸찾음/MainForm.cs
@@ -129,36 +129,7 @@ namespace 사진비교_비슷한걸찾음
 				return "비슷합니다."; // + count + size * (AllowableMarginOfError / 10);
 			}
 		}
-		//bool colorComparison(int[] Image1Color, int[] Image2Color){
-		//	int image1 = Image1Color[0] + Image1Color[1] + Image1Color[2];
-		//	int image2 = Image2Color[0] + Image2Color[1] + Image2Color[2];
-		//	if(image1 - image2 > (3 * comparisonNum) || image2 - image1 > (3 * comparisonNum)){
-		//		return false;
-		//	}
-		//	for(int a = 0; a < 3; a++){
-		//		if(Image1Color[a] - Image2Color[a] > (2 * comparisonNum) || Image2Color[a] - Image1Color[a] > (2 * comparisonNum)){
-		//			Console.WriteLine((Image1Color[a] - Image2Color[a]) +" "+ (Image2Color[a] - Image1Color[a]));
-		//			return false;
-		//		}
-		//	}
-		//	return true;
-		//}
 		
-		//int[] findColors(string input){ // Color.R, Color.G, Color.B로 표현할 수 있었네 아 ㅋㅋ
-		//	//색깔값은 Color [A=255, R=114, G=93, B=8] 이런식으로 되어있음
-		//	//얼마나 투명한지 표현하는 A값은 보지 않음 왜냐면 귀찮거든
-		//	int RPoint = input.IndexOf("R");
-		//	int GPoint = input.IndexOf("G");
-		//	int BPoint = input.IndexOf("B");
-		//	int R = int.Parse(input.Substring(RPoint+2, input.IndexOf(",", RPoint)-RPoint-2));
-		//	Console.WriteLine(R);
-		//	int G = int.Parse(input.Substring(GPoint+2, input.IndexOf(",", GPoint)-GPoint-2));
-		//	Console.WriteLine(G);
-		//	int B = int.Parse(input.Substring(BPoint+2, input.IndexOf("]", BPoint)-BPoint-2));
-		//	Console.WriteLine(B);
-		//	int[] Color = {R, G, B};
-		//	return Color;
-		//}
 		
 		Bitmap[] photoConversion(Bitmap image1, Bitmap image2){ //이미지 두개를 같은크기로 만들어줌
 			int _width = Math.Max(image1.Width, image2.Width);
@@ -202,28 +173,50 @@ namespace 사진비교_비슷한걸찾음
 			int colorG = 0;
 			int colorB = 0;
 			
-			if((color.R + color.G + color.B) / 3 > 200){
+			if(color.R > 200 && color.G > 200 && color.B > 200){
 				colorR = 255; // 하양
 				colorG = 255;
 				colorB = 255;
 			}
-			else if((color.R + color.G + color.B) / 3 < 25){
+			else if(color.R < 50 && color.G < 50 && color.B < 50){
 				colorR = 0; // 검정
 				colorG = 0;
 				colorB = 0;	
 			}
 			else if(color.R > color.G && color.R > color.B){
-				colorR = 255; // 빨강
+				if(color.R > color.G + color.B){
+					colorR = 255; // 빨강
+				}
+				else{
+					colorR = 255;
+					colorG = 100;
+					colorB = 100;
+				}
 			}
 			else if(color.G > color.R && color.G > color.B){
-				colorG = 255; // 초록
+				if(color.G > color.R + color.B){
+					colorR = 255; // 초록
+				}
+				else{
+					colorG = 255;
+					colorB = 100;
+					colorR = 100;
+				}
 			}
 			else{//if(colorB > colorG && colorB > colorR){
-				colorB = 255; // 파랑
+				if(color.B > color.G + color.R){
+					colorB = 255; // 빨강
+				}
+				else{
+					colorB = 255;
+					colorG = 100;
+					colorR = 100;
+				}
 			}
 			Color output = Color.FromArgb(colorR, colorG, colorB);
 			return output;
 		}
+		
 		int ReSize(int size){ //해상도를 낮추는 함수
 			if(size > 2000){
 				return (size / (20 * comparisonNum));
@@ -250,10 +243,42 @@ namespace 사진비교_비슷한걸찾음
 			return size;
 		}
 			
-		
 		void TextBox1TextChanged(object sender, EventArgs e)
 		{
 			
 		}
+	}
+	
+	class TrashCan{ //접으려고 만들어놓음
+		//bool colorComparison(int[] Image1Color, int[] Image2Color){
+		//	int image1 = Image1Color[0] + Image1Color[1] + Image1Color[2];
+		//	int image2 = Image2Color[0] + Image2Color[1] + Image2Color[2];
+		//	if(image1 - image2 > (3 * comparisonNum) || image2 - image1 > (3 * comparisonNum)){
+		//		return false;
+		//	}
+		//	for(int a = 0; a < 3; a++){
+		//		if(Image1Color[a] - Image2Color[a] > (2 * comparisonNum) || Image2Color[a] - Image1Color[a] > (2 * comparisonNum)){
+		//			Console.WriteLine((Image1Color[a] - Image2Color[a]) +" "+ (Image2Color[a] - Image1Color[a]));
+		//			return false;
+		//		}
+		//	}
+		//	return true;
+		//}
+		
+		//int[] findColors(string input){ // Color.R, Color.G, Color.B로 표현할 수 있었네 아 ㅋㅋ
+		//	//색깔값은 Color [A=255, R=114, G=93, B=8] 이런식으로 되어있음
+		//	//얼마나 투명한지 표현하는 A값은 보지 않음 왜냐면 귀찮거든
+		//	int RPoint = input.IndexOf("R");
+		//	int GPoint = input.IndexOf("G");
+		//	int BPoint = input.IndexOf("B");
+		//	int R = int.Parse(input.Substring(RPoint+2, input.IndexOf(",", RPoint)-RPoint-2));
+		//	Console.WriteLine(R);
+		//	int G = int.Parse(input.Substring(GPoint+2, input.IndexOf(",", GPoint)-GPoint-2));
+		//	Console.WriteLine(G);
+		//	int B = int.Parse(input.Substring(BPoint+2, input.IndexOf("]", BPoint)-BPoint-2));
+		//	Console.WriteLine(B);
+		//	int[] Color = {R, G, B};
+		//	return Color;
+		//}
 	}
 }
