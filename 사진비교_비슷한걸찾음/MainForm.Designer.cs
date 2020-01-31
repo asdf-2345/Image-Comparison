b52c5b646e5a911241593d4ed2594e5c82a933b3
asdf-2345 <kiaaa741@gmail.com>
2020-01-31 13:47:42 +0900

    아 푸시 잘못함 ㅋㅋ

--- 사진비교_비슷한걸찾음/MainForm.Designer.cs
+++ 사진비교_비슷한걸찾음/MainForm.Designer.cs
@@ -70,7 +70,7 @@ namespace 사진비교_비슷한걸찾음
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
+			this.button1.Text = "사진추가";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
@@ -80,7 +80,7 @@ namespace 사진비교_비슷한걸찾음
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 3;
-			this.button2.Text = "button2";
+			this.button2.Text = "사진추가";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.Button2Click);
 			// 
